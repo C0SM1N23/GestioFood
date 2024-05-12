e4540abd5e8f6dc15ff7b4a511354255d6a7894a
C0SM1N23 <bunea.cosmin2004@gmail.com>
2024-05-12 15:29:47 +0300

    test

--- GestioFoods/obj/Debug/net8.0-windows/GestioFoods.AssemblyInfo.cs
+++ GestioFoods/obj/Debug/net8.0-windows/GestioFoods.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("GestioFoods")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b19ed10deb302ca9ec03f623364ff0b44040e29f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3703b53b18e0cbc4d4bd4460fc1db15942173c70")]
 [assembly: System.Reflection.AssemblyProductAttribute("GestioFoods")]
 [assembly: System.Reflection.AssemblyTitleAttribute("GestioFoods")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
