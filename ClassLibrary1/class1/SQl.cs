14bf0965b8d787063921f89720b1a55e44242132
C0SM1N23 <bunea.cosmin2004@gmail.com>
2024-05-14 01:19:55 +0300

    commit puternic dezvoltat sql

---
+++ ClassLibrary1/class1/SQl.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassLibrary1.class1
+{
+    public class SQl
+    {
+        string connectionString = "Data Source=(LocalDb)COSMIN\\SQLEXPRESS01; Database=GestioFoods; Integrated Security= True";
+        public List<Product> TakeAllProducts()
+        {
+            string query = "SELECT p.*, f.ingredients, b.quantity " +
+                "FROM Product p" +
+                " LEFT JOIN food f ON p.ProductID = f.ProductID" +
+                "LEFT JOIN beverage h ON p.ProductID = b.ProductID";
+            List<Product> fetchedProducts = new List<Product>();
+            SqlConnection connection = new SqlConnection(connectionString);
+            try
+            {
+                connection.Open();
+                SqlCommand command = new SqlCommand(query, connection);
+                SqlDataReader reader = command.ExecuteReader();
+                while (reader.Read())
+                {
+                    int id = Convert.ToInt32(reader["ProductID"]);
+                    string name = reader["Name"].ToString();
+                    string type = reader["ProductType"].ToString();
+                    decimal price = Convert.ToDecimal(reader["Price"]);
+                    
+                    if (type == "Food")
+                    {
+                        string ingredients = reader["ingredients"].ToString();
+                        string platform = reader["Platform"].ToString();
+                        Food food= new Food(name, price , List<ingredients>) // !
+                        fetchedProducts.Add(food);
+                    }
+                    else if (type == "Beverage")
+                    {
+                        int quantity = Convert.ToInt32(reader["quantity"]);
+                        Beverage beverage = new Beverage( name, price , quantity );
+                        fetchedProducts.Add(beverage);
+                    }
+                }
+            }
+            finally
+            {
+                connection.Close();
+            }
+            return fetchedProducts;
+        }
+    }
+}
--- ClassLibrary1/obj/Debug/net8.0/ClassLibrary1.AssemblyInfo.cs
+++ ClassLibrary1/obj/Debug/net8.0/ClassLibrary1.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ClassLibrary1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+22c607c95c37c24a404ff2b50a490a4ee7563ed4")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b89470292d09b3b3ddb6f3799ef09c0c967360b1")]
 [assembly: System.Reflection.AssemblyProductAttribute("ClassLibrary1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ClassLibrary1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
